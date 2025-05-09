8474a4cac384715493529ed7b02ed93acbd541e8
mahmoud129856 <142356348+mahmoud129856@users.noreply.github.com>
2025-05-10 01:48:31 +0300

    s

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#app.py
 from flask import Flask, render_template, request, send_file, flash, redirect, url_for
 from PyPDF2 import PdfReader, PdfWriter
 from reportlab.pdfgen import canvas
@@ -126,5 +127,4 @@ def download_certificate():
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
